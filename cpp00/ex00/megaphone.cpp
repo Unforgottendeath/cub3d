cdbb89503f3bf98499496bf235c9c4d2144d10f9
Unforgottendeath <136029051+Unforgottendeath@users.noreply.github.com>
2024-06-30 21:43:17 +0000

    modifying makefile

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -4,10 +4,10 @@ int main(int ac, char **av)
 {
 	if (ac > 1)
 	{
-		int i {1};
+		int i = 1;
 		while ( av[i])
 		{
-			int j {};
+			int j = 0;
 			while (av[i][j])
 			{
 				if (av[i][j] >= 'a' && av[i][j] <= 'z')
